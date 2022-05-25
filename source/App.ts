70c3a1fed559feec5287d5cb6d82b6077ccf299a
Leonidas d'Assis <101950559+leonidasdassis0047@users.noreply.github.com>
2022-05-25 16:27:45 +0300

    Added the requires express app middlewares

--- source/App.ts
+++ source/App.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import compression from 'compression';
 
 export default class Application {
   private expressApp: express.Application = express();
 
-  constructor(private port: number) {}
+  constructor(private port: number) {
+    this.initializeAppMiddlewares();
+  }
 
   public startAppListening(): void {
     this.expressApp.listen(this.port, () => console.log('server running'));
+  }
+
+  private initializeAppMiddlewares(): void {
+    this.expressApp.use(helmet());
+    this.expressApp.use(compression());
+    this.expressApp.use(cors());
+    this.expressApp.use(morgan('common'));
+    this.expressApp.use(express.json());
+    this.expressApp.use(express.urlencoded({ extended: true }));
   }
 }
