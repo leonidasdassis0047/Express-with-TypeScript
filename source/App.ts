269e9ade330eff9406d5d080afb1c4e7d7c5742e
Leonidas d'Assis <101950559+leonidasdassis0047@users.noreply.github.com>
2022-05-25 16:15:45 +0300

    Created Express App and listened successfully

--- source/App.ts
+++ source/App.ts
@@ -1,1 +1,11 @@
-export default class Application {}
+import express from 'express';
+
+export default class Application {
+  private expressApp: express.Application = express();
+
+  constructor(private port: number) {}
+
+  public startAppListening(): void {
+    this.expressApp.listen(this.port, () => console.log('server running'));
+  }
+}
--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,7 @@
 import 'dotenv/config';
+
+import Application from './App';
+
+const PORT = Number(process.env.PORT) || 9000;
+const App = new Application(PORT);
+App.startAppListening();
