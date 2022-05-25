210edad0f7fd1472f73a5c97b6054c70333ba7b1
Leonidas d'Assis <101950559+leonidasdassis0047@users.noreply.github.com>
2022-05-25 16:34:31 +0300

    Initialized Database connection with the application

--- source/App.ts
+++ source/App.ts
@@ -3,12 +3,14 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import compression from 'compression';
+import mongoose from 'mongoose';
 
 export default class Application {
   private expressApp: express.Application = express();
 
   constructor(private port: number) {
     this.initializeAppMiddlewares();
+    this.initializeDatabaseConnection();
   }
 
   public startAppListening(): void {
@@ -22,5 +24,13 @@ export default class Application {
     this.expressApp.use(morgan('common'));
     this.expressApp.use(express.json());
     this.expressApp.use(express.urlencoded({ extended: true }));
+  }
+
+  private initializeDatabaseConnection(): void {
+    const { DATABASE_URI, DATABASE_NAME } = process.env;
+    mongoose
+      .connect(DATABASE_URI as string, { dbName: DATABASE_NAME as string })
+      .then((conn) => console.log(conn.connection.db.databaseName, 'DB'))
+      .catch((error: any) => console.error(error.message));
   }
 }
