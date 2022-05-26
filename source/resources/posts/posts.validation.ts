cc874cd8488eb38a3bd18a1b073e8c5b357738d4
Leonidas d'Assis <101950559+leonidasdassis0047@users.noreply.github.com>
2022-05-26 17:58:44 +0300

    Changed validation keys to required

--- source/resources/posts/posts.validation.ts
+++ source/resources/posts/posts.validation.ts
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 
 export const createPostValidator = Joi.object({
-  author: Joi.string().optional().trim().label('Post Author'),
-  content: Joi.string().optional().trim().label('Post Content'),
-  title: Joi.string().optional().trim().label('Post Title')
+  author: Joi.string().required().trim().label('Post Author'),
+  content: Joi.string().required().trim().label('Post Content'),
+  title: Joi.string().required().trim().label('Post Title')
 });
 
 export const updatePostValidator = Joi.object({
